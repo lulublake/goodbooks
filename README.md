a8698c0f015a9f05cab54b9bfab33c1c55cc1eb3
Oluchi Onyeguke <praiseonyeguke@gmail.com>
2025-05-18 16:09:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,100 @@
+# Book Review Website
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [Technologies Used](#technologies-used)
+  - [Frontend](#frontend)
+  - [Backend](#backend)
+  - [Database](#database)
+  - [APIs](#apis)
+  - [Deployment](#deployment)
+- [Features](#features)
+- [Setup and Installation](#setup-and-installation)
+- [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+This website is a personal book showcase where I display books I've read, share my reviews and ratings, and manage my reading list. Visitors can browse through the collection, read my thoughts on each book, and even add, edit, or delete book entries.
+
+### Screenshot
+![Screenshot](images/desktop-preview.png)
+
+### Links
+- Solution URL:
+- Live URL: https://github.com/lulublake/goodbooks.git
+
+## Technologies Used
+
+### Frontend
+- **Embedded Javascript (EJS)**: For structuring the web content, client-side interactivity and generating HTML markup with plain JavaScript.
+- **CSS**: For styling the website, including responsive design.
+
+### Backend
+- **Node.js**: The runtime environment for the server.
+- **Express.js**: Web application framework for Node.js, used to build the server-side of the application.
+
+### Database
+- **PostgreSQL**: An open-source object-relational database system used to store book information, reviews, and user data.
+- **Node-Postgres (pg)**: Non-blocking PostgreSQL client for Node.js, used to interact with the PostgreSQL database from the Node.js application.
+
+### APIs
+- **Open Library API**: Used to fetch book cover images and additional book metadata.
+
+### Deployment
+- **Render**: Cloud application hosting service used for deploying and hosting the application. Render provides easy deployment from Git repositories and supports PostgreSQL databases.
+
+## Features
+- Display of book collection with cover images, titles, and authors.
+- Sort books based on recency, title, and rating.
+- Search for specific books using the search bar.
+- Responsive design that works on both desktop and mobile devices.
+- Integration with Open Library API for book cover images.
+
+## Setup and Installation
+1. Create a database on pgAdmin and run the following lines of code:
+```sql
+CREATE TABLE read_books(
+  id SERIAL PRIMARY KEY,
+  isbn BIGINT UNIQUE NOT NULL,
+  cover_url TEXT,
+  title TEXT,
+  author TEXT,
+  review TEXT NOT NULL,
+  date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  rating INTEGER NOT NULL
+);
+```
+2. Clone the repository:
+```bash
+git clone https://github.com/lulublake/goodbooks.git
+```
+3. Install the dependencies:
+```bash
+npm install
+```
+4. Create a .env file in the root of the project and add the code:
+```bash
+DATABASE_URL="postgres://your_local_db_username:your_password@localhost:5432/name_of_database"
+```
+5. Run the application:
+```bash
+node --watch index.js
+```
+6. Usage:
+Visit http://localhost:3000 in your browser to view the website.
+
+## Useful Resources
+- [Derek Sivers Book Notes](https://sive.rs/book) — Used for layout inspiration.
+- [Digital Bookshelf by Dimitris Papazafeiris](https://github.com/DimitrisPapazafeiris/Digital-Bookself) — Helpful reference when working with the Open Library API.
+- [Open Library Covers API](https://openlibrary.org/dev/docs/api/covers) — Used for fetching book cover images.
+
+## Author
+- [X (Twitter)](https://x.com/__babylu)
+- [Instagram](https://www.instagram.com/_baby.lu__)
+
+## Acknowledgements
+I would like to thank Dr. Angela Yu, the instructor to my fullstack course at Udemy and for giving me such a nice capstone project to showcase my skills on my Portfolio.
---
+++ index.js
@@ -0,0 +1,200 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+import pg from "pg";
+import dotenv from 'dotenv';
+
+const app = express();
+const port = process.env.PORT || 3000;
+dotenv.config();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// const db = new pg.Client({
+//     user: 'pg_admin_username',
+//     password: 'pg_admin_password',
+//     database: 'name_of_db',
+//     host: 'localhost',
+//     port: 5432
+// });
+// db.connect();
+
+const isRender = process.env.DATABASE_URL?.includes("render.com");
+
+const db = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: isRender ? { rejectUnauthorized: false } : false,
+});
+db.connect();  
+
+async function allBooks(){
+    const result = await db.query('SELECT * FROM read_books ORDER BY id ASC');
+    const books = [];
+    result.rows.forEach((book) => {
+        books.push(book);
+    });
+    return books;
+}
+
+app.get('/', async (req, res) =>{
+    const books = await allBooks();
+    res.render("index.ejs", { books: books });
+});
+
+app.get('/sort-option', async(req, res) => {
+    const selectedOption = req.query.options;
+    const books = [];
+
+    let query = "SELECT * FROM read_books";
+
+    if (selectedOption === "rating") {
+      query += " ORDER BY rating DESC";
+    } else if (selectedOption === "date") {
+      query += " ORDER BY date_added DESC";
+    } else if (selectedOption === "title") {
+      query += " ORDER BY title ASC";
+    }
+
+    const result = await db.query(query);
+    result.rows.forEach((book) => {
+        books.push(book);
+    });
+
+    res.render("index.ejs", { 
+        books: books,
+        selected: selectedOption 
+    });
+});
+
+app.post('/search', async(req, res) => {
+    const bookName = req.body.search.trim();
+    const newBooks = [];
+    try {
+        const result = await db.query(
+            'SELECT * FROM read_books WHERE title ILIKE $1',
+            [`%${bookName}%`]
+        );
+        result.rows.forEach((book) => {
+            newBooks.push(book);
+        });
+        res.render('index.ejs', {
+            books: newBooks
+        });
+    } catch (error) {
+        const books = await allBooks();
+        res.render('index.ejs', {
+            books: books,
+            error: "No book found"
+        });
+        console.log(error);
+    }
+     
+});
+
+app.post('/add', async(req, res) => {
+    res.render("newbook.ejs");
+});
+
+app.post('/edit', async(req, res) => {
+    const bookId = req.body.bookID;
+    const result = await db.query('SELECT * FROM read_books WHERE id = $1', [bookId]);
+    const book = result.rows[0];
+    res.render("oldbook.ejs", { book: book });
+});
+
+app.post('/new', async(req, res) => {
+    const isbn = req.body.isbn;
+    const review = req.body.review;
+    const rating = parseFloat(req.body.rating);
+
+    try {
+        const apiRequest = await axios.get(
+            `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json&jscmd=data`
+        );
+        const bookData = apiRequest.data[`ISBN:${isbn}`];
+        const title = bookData.title;
+        const author = bookData.authors[0].name;
+        const cover_url = bookData.cover.medium;
+
+        const alreadyExists = await db.query(
+            'SELECT * FROM read_books WHERE isbn = $1',
+            [isbn]
+        );
+
+        if(alreadyExists.rows.length > 0){
+            return res.render('newbook.ejs', {
+                isbn_error: 'You already have this book'
+            });
+        }
+
+        if(!review){
+            return res.render('newbook.ejs', { error: 'This field is required' });
+        }
+
+        if(!rating || rating < 1 || rating > 10){
+            return res.render('newbook.ejs', { rating_error: 'Rating must be between 1 and 10'});
+        }
+
+        try {
+            await db.query(
+                'INSERT INTO read_books(isbn, cover_url, title, author, review, rating) VALUES ($1, $2, $3, $4, $5, $6)',
+                [isbn, cover_url, title, author, review, rating]
+            );
+            res.redirect('/');
+        } catch (error) {
+            console.log(error)
+            res.status(500).send('Failed to add book.');
+        }
+         
+    } catch (isbn_error) {
+        res.render('newbook.ejs', {
+            isbn_error: 'Book not found'
+        });
+    }
+});
+
+app.post('/update', async(req, res) => {
+    const bookId = req.body.bookID;
+    const review = req.body.review;
+    const rating = parseFloat(req.body.rating);
+
+    try {
+        const result = await db.query('SELECT * FROM read_books WHERE id = $1', [bookId]);
+        const bookDetails = result.rows[0];
+
+        if(!review){
+            review = bookDetails.review;
+        }
+        if(!rating){
+            rating = bookDetails.rating;
+        }
+
+        await db.query(
+            'UPDATE read_books SET review = $1, rating = $2 WHERE id = $3',
+            [review, rating, bookId]
+        );
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Failed to update book.');
+    }
+});
+
+app.post('/delete', async(req, res) => {
+    const bookId = req.body.bookID;
+    try {
+        await db.query(
+            'DELETE FROM read_books WHERE id = $1',
+            [bookId]
+        );
+        res.redirect('/');
+    } catch (error) {
+       console.log(error); 
+    }
+});
+
+app.listen(port, () =>{
+    console.log(`Started listening at port ${port}`);
+});
+
